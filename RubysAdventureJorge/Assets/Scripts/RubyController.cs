fd16bc35544592c38dd9429a85275f95f81ffe5c
Jorge <719727@student.dusd.net>
2022-11-30 11:43:51 -0800

    Finished decor
    finished decor now just scripting

--- RubysAdventureJorge/Assets/Scripts/RubyController.cs
+++ RubysAdventureJorge/Assets/Scripts/RubyController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,12 +19,14 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+    } 
 
+    void FixedUpdate()
+    { 
 
-        Debug.Log(horizontal);
         Vector2 position = transform.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+       rigidbody2d.MovePosition(position);
     }
 }
